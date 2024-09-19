9fa193d967153e95af65d476dcbe17c966b2c389
mahamjabbar456 <mahamjabbar320@gmail.com>
2024-09-19 17:35:21 +0500

    In this project i use useRef,useEffect and useState hook. I use alert dialog component to make this is more beautiful

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Pomodoro_Timer from "@/components/Pomodoro_Timer";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Pomodoro_Timer />
+    </div>
+  );
+}
